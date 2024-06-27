dba939eae289a305a7708956e1bbcd1a12c3ac1f
0binny0 <binnyhh@protonmail.com>
2024-06-27 10:15:08 -0700

    installed vitest config

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup.js"],
+    fakeTimers: {
+      shouldAdvanceTime: true,
+      advanceTimeDelta: 500
+    }
+  }
+})
