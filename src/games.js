f339ed5a21490d8de7466a7baf142b9171eff1d5
0binny0 <binnyhh@protonmail.com>
2024-06-27 22:50:26 -0700

    added presentation of game memory store

---
+++ src/games.js
@@ -0,0 +1,71 @@
+
+export const games = [[
+    ["red"], ["red", "red"], ["red", "red", "yellow"], ["red", "red", "yellow", "green"],
+    ["red", "red", "yellow", "green", "red"], ["red", "red", "yellow", "green", "red", "blue"],
+    ["red", "red", "yellow", "green", "red", "blue", "yellow"],
+    ["red", "red", "yellow", "green", "red", "blue", "yellow", "green"],
+    ["red", "red", "yellow", "green", "red", "blue", "yellow", "green", "blue"],
+    ["red", "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", "blue"],
+    ["red", "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", "blue", "red"],
+    ["red", "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", "blue", "red", "blue"],
+    ["red", "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", "blue", "red", "blue", "yellow"],
+    ["red", "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", "blue", "red", "blue", "yellow", "blue"],
+    ["red", "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", "blue", "red", "blue", "yellow", "blue",
+    "green"], ["red", "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", "blue", "red", "blue",
+    "yellow", "blue", "green", "red"]
+], [
+    ["red"], ["red", "blue"], ["red", "blue", "green"], ["red", "blue", "green", "red"], ["red", "blue", "green", "red", "blue"],
+    ["red", "blue", "green", "red", "blue", "blue"], ["red", "blue", "green", "red", "blue", "blue", "green"],
+    ["red", "blue", "green", "red", "blue", "blue", "green", "blue"],
+    ["red", "blue", "green", "red", "blue", "blue", "green", "blue", "red"],
+    ["red", "blue", "green", "red", "blue", "blue", "green", "blue", "red", "green"],
+    ["red", "blue", "green", "red", "blue", "blue", "green", "blue", "red", "green", "yellow"],
+    ["red", "blue", "green", "red", "blue", "blue", "green", "blue", "red", "green", "yellow", "blue"],
+    ["red", "blue", "green", "red", "blue", "blue", "green", "blue", "red", "green", "yellow", "blue", "green"],
+    ["red", "blue", "green", "red", "blue", "blue", "green", "blue", "red", "green", "yellow", "blue", "green", "yellow"],
+    ["red", "blue", "green", "red", "blue", "blue", "green", "blue", "red", "green", "yellow", "blue", "green", "yellow",
+    "green"], ["red", "blue", "green", "red", "blue", "blue", "green", "blue", "red", "green", "yellow", "blue",
+    "green", "yellow", "green", "red"]
+], [
+    ["yellow"], ["yellow", "green"], ["yellow", "green", "yellow"], ["yellow", "green", "yellow", "red"],
+    ["yellow", "green", "yellow", "red", "red"], ["yellow", "green", "yellow", "red", "red", "blue"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue", "red"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue", "red", "yellow"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue", "red", "yellow", "blue"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue", "red", "yellow", "blue", "green"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue", "yellow", "blue", "green", "green"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue", "yellow", "blue", "green", "green", "red"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue", "yellow", "blue", "green", "green", "red", "yellow"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue", "yellow", "blue", "green", "green", "red", "yellow", "blue"],
+    ["yellow", "green", "yellow", "red", "red", "blue", "blue", "yellow", "blue", "green", "green", "red", "yellow",
+    "blue", "blue"], ["yellow", "green", "yellow", "red", "red", "blue", "blue", "yellow", "blue", "green", "green",
+    "red", "yellow", "blue", "blue", "green"]
+
+], [
+    ["green"], ["green", "blue"], ["green", "blue", "green"], ["green", "blue", "green", "yellow"],
+    ["green", "blue", "green", "yellow", "yellow"], ["green", "blue", "green", "yellow", "yellow", "red"],
+    ["green", "blue", "green", "yellow", "yellow", "red", "blue"],
+    ["green", "blue", "green", "yellow", "yellow", "red", "blue", "yellow"],
+    ["green", "blue", "green", "yellow", "yellow", "red", "blue", "yellow", "yellow"],
+    ["green", "blue", "green", "yellow", "yellow", "red", "blue", "yellow", "yellow", "green"],
+    ["green", "blue", "green", "yellow", "yellow", "red", "blue", "yellow", "yellow", "green", "blue"],
+    ["green", "blue", "green", "yellow", "yellow", "red", "blue", "yellow", "yellow", "green", "blue", "blue"],
+    ["green", "blue", "green", "yellow", "yellow", "red", "blue", "yellow", "yellow", "green", "blue", "blue", "red"],
+    ["green", "blue", "green", "yellow", "yellow", "red", "blue", "yellow", "yellow", "green", "blue", "blue",
+    "red", "green"], ["green", "blue", "green", "yellow", "yellow", "red", "blue", "yellow", "yellow", "green", "blue",
+    "blue", "red", "green", "yellow"]
+], [
+    ["blue"], ["blue", "green"], ["blue", "green", "red"], ["blue", "green", "red", "red"],
+    ["blue", "green", "red", "red", "yellow"], ["blue", "green", "red", "red", "yellow", "red"],
+    ["blue", "green", "red", "red", "yellow", "red", "blue"], ["blue", "green", "red", "red", "yellow", "red", "blue", "blue"],
+    ["blue", "green", "red", "red", "yellow", "red", "blue", "blue", "yellow"],
+    ["blue", "green", "red", "red", "yellow", "red", "blue", "blue", "yellow", "green"],
+    ["blue", "green", "red", "red", "yellow", "red", "blue", "blue", "yellow", "green", "red"],
+    ["blue", "green", "red", "red", "yellow", "red", "blue", "blue", "yellow", "green", "red", "yellow"],
+    ["blue", "green", "red", "red", "yellow", "red", "blue", "blue", "yellow", "green", "red", "yellow", "blue"],
+    ["blue", "green", "red", "red", "yellow", "red", "blue", "blue", "yellow", "green", "red", "yellow", "blue", "red"],
+    ["blue", "green", "red", "red", "yellow", "red", "blue", "blue", "yellow", "green", "red", "yellow", "blue", "red", "red"],
+    ["blue", "green", "red", "red", "yellow", "red", "blue", "blue", "yellow", "green", "red", "yellow", "blue", "red",
+    "red", "green"]
+]];
