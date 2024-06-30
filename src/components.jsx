1eb1e746a16a7b2855b297baa440c6792c0bc33f
0binny0 <binnyhh@protonmail.com>
2024-06-30 14:18:53 -0700

    tested start_new_game upon clicking the start button

--- src/components.jsx
+++ src/components.jsx
@@ -5,20 +5,79 @@ import {games} from "./games.js";
 
 function Simon(props) {
     // Indicates the level of difficulty when progressing through the sequences of the game
-    const [level, setLevel] = useState(1);
+    const [level, setLevel] = useState("A");
     //Represents the power button to turn the game on or off
     const [power, setPower] = useState(false);
     //The collection of successive games to progress through
     const [game, setGame] = useState(games[Math.floor(Math.random() * games.length)]);
     // The sequence of colors to match before advancing to a more difficult sequence
-    const round = game[level];
+    if (!isNaN(level)) {
+        //Sets the current sequence to play
+        const round = game[parseInt(level)];
+        //Refers to the remaining colors to match in the sequence
+        const colors_remaining = JSON.parse(JSON.stringify(round));
+    }
     //Reverts the player to the beginning of the game if the player loses (optional)
     const [strictGame, setStrictGame] = useState(false);
     //References the remaining colors to match for the given round
-    const colors_remaining = JSON.parse(JSON.stringify(round));
     const timer = useRef(null);
     //The time limit set to expire if a pad is not clicked (in seconds)
     const time_limit = useRef(5);
+
+    function turn_simon_on() {
+        //Clicking the "power" button will enable the player to
+        //start a new game and/or setting the game to strict mode
+        setPower(!power);
+    }
+
+    function start_new_game() {
+        //When "start" is clicked the game display will
+        //blink "--" until the game is ready to play
+        let i = 0;
+        const indicators =  [undefined, "--", undefined, "0"];
+        const interval_id = setInterval(
+            () => {
+                const indicator = indicators[i];
+                setLevel(indicator);
+                if (indicator === "0") {
+                    clearInterval(interval_id);
+                } else {
+                    i++;
+                }
+            }, 500
+        );
+    }
+
+    const pads = ["red", "green", "blue", "yellow"].map((color) => {
+        return <Pad key={crypto.randomUUID()} isOn={power} color={color} changeLevel={setLevel} changeGame={setGame} />
+    });
+
+    return <main>
+        <div className="game_pads">
+            {pads}
+        </div>
+        <section className="interface">
+            <h1 className="game_name">Simon</h1>
+            <div className="display">
+                <p className="level_display">{power ? isNaN(level) ? level : parseInt(level) + 1 : null}</p>
+                <p className="title">Level</p>
+            </div>
+            <div className="start_button_wrapper">
+                <button type="button" className="start_button" id="start_game" aria-label="start_new_game" onClick={start_new_game}></button>
+                <p className="title">Start</p>
+            </div>
+            <div className="power_buttons_wrapper">
+                <p className="title">Off</p>
+                <button className={!power ? "power_button power_off" : "power_button power_on"} onClick={turn_simon_on} aria-label="turn_game_off"></button>
+                <button className={power ? "power_button power_on" : "power_button power_off"} onClick={turn_simon_on} aria-label="turn_game_on"></button>
+                <p className="title">On</p>
+            </div>
+            <div className="strict_game_wrapper">
+                <div className="strict_light_indicator"></div>
+                <p className="title">Strict</p>
+            </div>
+        </section>
+    </main>
 }
 
 function Pad({isOn, color, changeLevel, changeGame}) {
--- src/components.test.jsx
+++ src/components.test.jsx
@@ -1,12 +1,20 @@
 
-import {test, expect, vi} from "vitest";
+import {test, expect, vi, beforeEach, afterEach} from "vitest";
 
-import {render, screen} from "@testing-library/react";
+import {render, screen, waitFor} from "@testing-library/react";
 import userEvent from '@testing-library/user-event'
 
 import {Simon, Pad} from "./components.jsx";
 
-const user = userEvent.setup();
+
+beforeEach(() => {
+    vi.useFakeTimers();
+});
+
+afterEach(() => {
+    vi.runOnlyPendingTimers();
+    vi.useRealTimers();
+});
 
 test(
     `Verify that a pad receives a CSS color when the Simon game is turned on`
@@ -15,5 +23,25 @@ test(
         const pad = screen.getByRole("button", {name: "blue_pad"});
         expect(pad).not.toHaveClass("power_blue_pad_off");
         expect(pad).toHaveClass("power_blue_pad_on");
+    }
+);
+
+
+test(
+    `
+    Verify that clicking the start button (when the game is on) displays on the
+    screen the beginning level (Level 1)
+
+    See:
+
+    * https://github.com/testing-library/react-testing-library/issues/1198
+    * https://github.com/testing-library/user-event/issues/1115
+    `, async () => {
+        let user = userEvent.setup({advanceTimers: () => vi.advanceTimersToNextTimer});
+        render(<Simon />);
+        await user.click(screen.getByRole("button", {name: "turn_game_on"}));
+        await user.click(screen.getByRole("button", {name: "start_new_game"}));
+        expect(await screen.findByText("--")).toBeVisible();
+        expect(await screen.findByText("1")).toBeVisible();
     }
 );
